a3f4609e114ba9836b8b58c350c51a0e784e209f
ozen-hub <info.ozenhub@gmail.com>
2023-01-03 22:11:38 +0530

    Initial commit

---
+++ src/main/java/com/developersatck/security/api/CustomerController.java
@@ -0,0 +1,18 @@
+package com.developersatck.security.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+    @PostMapping
+    public String createCustomer(){return "createCustomer";}
+    @GetMapping
+    public String findCustomer(){return "findCustomer";}
+    @DeleteMapping
+    public String deleteCustomer(){return "deleteCustomer";}
+    @PutMapping
+    public String updateCustomer(){return "updateCustomer";}
+    @GetMapping("/list")
+    public String getAllCustomers(){return "getAllCustomers";}
+}
---
+++ src/test/java/com/developersatck/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developersatck.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
