b8d56f456b38995c1e3b520263054d126e369df4
ozen-hub <info.ozenhub@gmail.com>
2023-01-03 23:28:28 +0530

    3

--- src/main/java/com/developersatck/security/config/security/ApplicationSecurityConfig.java
+++ src/main/java/com/developersatck/security/config/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.developersatck.security.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -11,11 +16,28 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.
-               authorizeRequests()
-               .anyRequest()
-               .authenticated()
-               .and()
-               .httpBasic();
+        http.
+                authorizeRequests()
+                .antMatchers("/", "index")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+
+        UserDetails tharaka = User.builder()
+                .username("tharaka")
+                .password("1234")
+                .roles("CUSTOMER")
+                .build();
+        return new InMemoryUserDetailsManager(
+                tharaka
+        );
+
     }
 }
