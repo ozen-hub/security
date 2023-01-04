c7591731ad4e65b7fcf60d07516d5edd4a50c7ff
ozen-hub <info.ozenhub@gmail.com>
2023-01-04 23:10:52 +0530

    13

--- src/main/java/com/developersatck/security/api/ProductController.java
+++ src/main/java/com/developersatck/security/api/ProductController.java
@@ -1,18 +1,24 @@
 package com.developersatck.security.api;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member/api/v1/products")
 public class ProductController {
     @PostMapping
+    @PreAuthorize("hasAuthority('product:write')")
     public String createProduct(){return "createProduct";}
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MANAGER')")
     public String findProduct(){return "findProduct";}
     @DeleteMapping
+    @PreAuthorize("hasAuthority('product:write')")
     public String deleteProduct(){return "deleteProduct";}
     @PutMapping
+    @PreAuthorize("hasAuthority('product:write')")
     public String updateProduct(){return "updateProduct";}
     @GetMapping("/list")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MANAGER')")
     public String getAllProducts(){return "getAllProducts";}
 }
