9001588db8a651c955b3d126d5cf891f030af2c8
ozen-hub <info.ozenhub@gmail.com>
2023-01-06 22:46:26 +0530

    25

--- src/main/java/com/developersatck/security/jwt_config/JwtAuthenticationFilter.java
+++ src/main/java/com/developersatck/security/jwt_config/JwtAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.developersatck.security.jwt_config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -8,9 +10,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -23,7 +29,6 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-
         try {
             UsernameAndPasswordRequest usernameAndPasswordRequest =
                     new ObjectMapper()
@@ -33,12 +38,26 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                     usernameAndPasswordRequest.getUsername(),
                     usernameAndPasswordRequest.getPassword()
             );
-            authenticationManager.authenticate(authentication);
-
+            return authenticationManager.authenticate(authentication);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        return super.attemptAuthentication(request, response);
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        String key = "lskdjuiioferuemorujfteupotufougnruyouyjgoprtjgoujogiutoiurtyytyrty";
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(31)))
+                .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                .compact();
+
+        response.addHeader("Authorization", "Bearer " + token);
     }
 }
