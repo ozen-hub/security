61a52fffe9ceb3a98147717d2cf30ff95494b277
ozen-hub <info.ozenhub@gmail.com>
2023-01-06 22:14:45 +0530

    23

---
+++ src/main/java/com/developersatck/security/jwt_config/JwtAuthenticationFilter.java
@@ -0,0 +1,15 @@
+package com.developersatck.security.jwt_config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        return super.attemptAuthentication(request, response);
+    }
+}
